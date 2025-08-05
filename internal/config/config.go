4f1f922d4687f611baa7d65451cc4a1f1d40ec97
Nathan Fabio <nathanfabio.dev@gmail.com>
2025-08-05 15:25:11 -0300

    Add MaxWorkers configuration to Load function in Config struct, enabling dynamic worker pool size based on environment variable.

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -11,14 +12,18 @@ type Config struct {
 	RedisURL             string
 	Port                 string
 	HealthCheckInterval  time.Duration
+	MaxWorkers           int
 }
 
 func Load() *Config {
+	maxWorkers, _ := strconv.Atoi(os.Getenv("MAX_WORKERS"))
+
 	return &Config{
 		DefaultProcessorURL:  os.Getenv("PAYMENT_PROCESSOR_URL_DEFAULT"),
 		FallbackProcessorURL: os.Getenv("PAYMENT_PROCESSOR_URL_FALLBACK"),
 		RedisURL:             os.Getenv("REDIS_URL"),
 		Port:                 os.Getenv("APP_PORT"),
 		HealthCheckInterval:  5 * time.Second,
+		MaxWorkers:           maxWorkers,
 	}
 }
